3433995d410a69927b7ea6e3843df3f5bd32ee11
diana.josan <diana.josan@3pillarglobal.com>
2021-11-29 10:23:41 +0200

    Solve comments for exercises 7, 8 & 9.

--- HighsAndLows.py
+++ HighsAndLows.py
@@ -2,11 +2,10 @@ my_list = [20, 9, 51, 81, 50, 42, 77]
 
 
 def count_low_high(my_list):
-    high_list = len([n for n in my_list if n > 50 or n % 3 == 0])
-    low_list = len([n for n in my_list if n <= 50 and not n % 3 == 0])
-
     if (len(my_list) == 0):
         return None
+    high_list = len([n for n in my_list if n > 50 or n % 3 == 0])
+    low_list = len(my_list) - high_list
 
     return [low_list, high_list]
 
