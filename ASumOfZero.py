097a1f7da4d7daa195cebf62d29d49aa9bd1fe36
diana.josan <diana.josan@3pillarglobal.com>
2021-12-03 13:32:53 +0200

    Rewrite the sum of zero problem.

--- ASumOfZero.py
+++ ASumOfZero.py
@@ -1,13 +1,12 @@
-my_list = [10, -14, 26, 5, -3, 13, -5]
+my_list = [10, -14, 26, -3, 13, -4, -1, 5]
 
 
 def check_sum(my_list):
     for n in range(0, len(my_list)):
-        for n1 in range(n + 1, len(my_list)):
-            if my_list[n] + my_list[n1] == 0:
-                return True
+        total = 0
+        for n1 in range(n, len(my_list)):
+            total += my_list[n1]
+            if total == 0:
+                print("We found the elements with sum zero.")
 
-    return False
-
-
-print("We have 2 numbers in the list with sum zero. (" + str(check_sum(my_list)) + ")")
+check_sum(my_list)
